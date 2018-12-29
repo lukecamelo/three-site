e4c4c24d775e73aea038c740329edf8160c4c140
Luke Camelo <azathothbeats@gmail.com>
2018-12-28 20:20:08 -0500

    Organized initialization code into init function

--- index.js
+++ index.js
@@ -1,23 +1,38 @@
-const scene = new THREE.Scene()
-const camera = new THREE.PerspectiveCamera(
-  75,
-  window.innerWidth / window.innerHeight,
-  0.1,
-  1000
-)
+let renderer, scene, camera, container
 
-const renderer = new THREE.WebGLRenderer()
-renderer.setSize(window.innerWidth, window.innerHeight)
-document.body.appendChild(renderer.domElement)
+function init() {
+  container = document.createElement('div')
+  document.body.appendChild(container)
 
+  camera = new THREE.PerspectiveCamera(
+    75, // view angle
+    window.innerWidth / window.innerHeight, // Aspect ratio (?)
+    0.1, // near
+    1000 // far
+  )
+  camera.position.set(0, 0, 10)
+  camera.lookAt(0, 0, 0)
+
+  scene = new THREE.Scene()
+  renderer = new THREE.WebGLRenderer()
+
+  renderer.setSize(window.innerWidth, window.innerHeight)
+  container.appendChild(renderer.domElement)
+}
+
+init()
 const geometry = new THREE.BoxGeometry(1, 1, 1)
 const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+
 const cube = new THREE.Mesh(geometry, material)
 scene.add(cube)
-camera.position.z = 5
 
 function animate() {
   requestAnimationFrame(animate)
+
+  cube.rotation.x += 0.01
+  cube.rotation.y += 0.01
+
   renderer.render(scene, camera)
 }
 
